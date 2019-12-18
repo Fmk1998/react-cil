427bc841c7c4b23b8bd893f89cc4465451418755
huangxi <huangq@paraview.cn>
2019-12-18 18:57:27 +0800

    task

--- src/components/common/Task/SimpleList.tsx
+++ src/components/common/Task/SimpleList.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import {createStyles, Theme, makeStyles} from '@material-ui/core/styles';
 import {
     Avatar,
-    IconButton,
     List,
     ListItem,
     ListItemAvatar,
-    ListItemSecondaryAction,
-    ListItemText
+    ListItemText,
+    ExpansionPanel,
+    ExpansionPanelDetails,
+    ExpansionPanelSummary,
+    Typography
 } from "@material-ui/core";
-import {Delete,Folder} from '@material-ui/icons'
+import {ExpandMoreSharp,PlaylistPlay} from '@material-ui/icons'
 import {FormattedHTMLMessage} from 'react-intl'
+import './Task.scss'
 
 interface Props {
     list: []
@@ -27,26 +30,40 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function SimpleList(props: Props) {
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState<number | false>(0);
+
+    const handleChange = (panel: number) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => {
+        setExpanded(isExpanded ? panel : false);
+    }
+
     return (
         <div className={classes.root}>
-            <List component="nav" aria-label="secondary mailbox folders">
-                {props.list.length > 0
-                    ? props.list.map((value: { id: string, name: string }) => (
-                        <ListItem button key={value.id}>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <Folder />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={value.name}/>
-                            <ListItemSecondaryAction>
-                                <IconButton edge="end" aria-label="delete">
-                                    <Delete/>
-                                </IconButton>
-                            </ListItemSecondaryAction>
-                        </ListItem>
-                    )) : <FormattedHTMLMessage id={"noData"}/>}
-            </List>
+            {props.list.length > 0
+                ? props.list.map((task: { id: string, name: string, annex: string }, index: number) => (
+                    <ExpansionPanel expanded={expanded === index} onChange={handleChange(index)} key={task.id}>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreSharp/>} className="task-header">
+
+                            <Typography>{task.name}</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <List>
+                                {
+                                    JSON.parse(task.annex).map((docs: { label: string, value: string }) => (
+                                        <ListItem key={docs.label}>
+                                            <ListItemAvatar>
+                                                <Avatar/>
+                                            </ListItemAvatar>
+                                            <ListItemText primary={docs.value}/>
+                                        </ListItem>
+                                    ))
+                                }
+                            </List>
+
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                ))
+                : <FormattedHTMLMessage id={"noData"}/>
+            }
         </div>
-    );
+    )
 }
