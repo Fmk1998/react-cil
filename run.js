f77ffd2e9908adf6acc972309054df45401d68f6
hklmtt <liujia307@qq.com>
2020-02-18 13:54:26 +0800

    修复windows下无法运行的问题

--- run.js
+++ run.js
@@ -1,31 +1,10 @@
 const {dev, version} = require('./package');
-const {spawn} = require('child_process');
-const {promises: fs} = require('fs');
+const fs = require('fs');
 const env = process.argv[2] || '';// dev 调试模式
 
-const runReact = () => {
-    let ls;
-    if (env === 'dev')
-        ls = spawn('cross-env', [`PORT=${dev.port || 8088}`, 'react-scripts', 'start']);
-    else
-        ls = spawn('react-scripts', ['build']);
-
-    ls.stdout.on('data', (data) => {
-        console.log(data.toString());
-    });
-
-    ls.stderr.on('data', (data) => {
-        console.error(data.toString());
-    });
-
-    ls.on('close', (code) => {
-        console.log(`子进程退出，退出码 ${code}`);
-    });
-};
-
 const indexPath = './src/index.tsx';
-const init = async () => {
-    const data = await fs.readFile(indexPath);
+const init = () => {
+    const data = fs.readFileSync(indexPath);
     const index = data.toString().replace(/(\/\* @dynamic (.*?) \*\/\n*\n*\r*\s*\t*)(.*?)(\n*\n*\r*\s*\t*\/\* @dynamic end \*\/)/gi, (a, s1, s2, s3, s4) => {
         let code = '';
         switch (s2) {
@@ -45,8 +24,7 @@ const init = async () => {
         return `${s1}${code}${s4}`;
     });
     try {
-        await fs.writeFile(indexPath, index);
-        runReact();
+        fs.writeFileSync(indexPath, index);
     } catch (err) {
         console.log(err);
     }
