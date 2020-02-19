59dc7248dc89ace3fb87e2bfadd903cb3aefe560
chenxi <chenxihub@163.com>
2020-02-19 10:40:44 +0800

    国际化

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import {Debugger} from 'para-lib';
 /* @dynamic end */
 declare let window: Window & { ParaWeb: any };
 /* @dynamic version */
-window.ParaWeb = {version: '0.1.0', env: 'dev', buildTime: '2/19/2020, 2:22:07 AM'}
+window.ParaWeb = {version: '0.1.0', env: 'dev', buildTime: '2/19/2020, 10:21:23 AM'}
 /* @dynamic end */
 
 const init = async (debug: Array<string> = []) => {
