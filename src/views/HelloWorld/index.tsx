9f54e189cb88feac37ed018dce3977ab0d1eaa3c
chenxi <chenxihub@163.com>
2020-02-19 19:13:49 +0800

    Merge branch 'master' of ssh://gitlab.paraview.cn:2222/Frontend/react-cli
    # Conflicts:
    #	src/App.tsx
    #	src/index.tsx

--- src/views/HelloWorld/index.tsx
+++ src/views/HelloWorld/index.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent} from 'react';
-import Button from 'para-ui/core/Button';
+import Button from '@material-ui/core/Button';
 import {withRouter, RouteComponentProps} from 'react-router-dom'
 import './index.scss';
 
