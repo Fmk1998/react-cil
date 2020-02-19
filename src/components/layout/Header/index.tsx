fef3d8a8807d71628eb075ff31cad4f530dbdbc2
huangxi <huangq@paraview.cn>
2020-02-19 10:43:21 +0800

    header

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import {AppBar} from "para-ui/core"
+
 import './index.scss';
 
 interface Props {
-
+    route: number[]
 }
 
 interface State {
+
 }
 
 
-export default class Header extends React.Component<Props, State> {
+export default class Header extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props)
+    }
 
     render(): React.ReactNode {
         return (
--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import {Debugger} from 'para-lib';
 /* @dynamic end */
 declare let window: Window & { ParaWeb: any };
 /* @dynamic version */
-window.ParaWeb = {version: '0.1.0', env: 'dev', buildTime: '2/19/2020, 2:22:07 AM'}
+window.ParaWeb = {version: '0.1.0', env: 'dev', buildTime: '2020-2-19 10:11:04'}
 /* @dynamic end */
 
 const init = async (debug: Array<string> = []) => {
