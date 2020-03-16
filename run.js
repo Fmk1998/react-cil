eba64cc6e5cd51dc95a3753cc29a6da0cd349cbd
chenxi <chenxihub@163.com>
2020-03-16 14:49:26 +0800

    修改run.js

--- run.js
+++ run.js
@@ -1,3 +1,4 @@
+/* v 0.0.2 */
 const {dev, version} = require('./package');
 const fs = require('fs');
 const env = process.argv[2] || '';// dev 调试模式
@@ -5,26 +6,26 @@ const env = process.argv[2] || '';// dev 调试模式
 const indexPath = './src/index.tsx';
 const data = fs.readFileSync(indexPath);
 const index = data.toString().replace(/(\/\* @dynamic (.*?) \*\/\n*\n*\r*\s*\t*)(.*?)(\n*\n*\r*\s*\t*\/\* @dynamic end \*\/)/gi, (a, s1, s2, s3, s4) => {
-    let code = '';
-    switch (s2) {
-        case 'debug':
-            code = env === 'dev' ? `import {Debugger} from 'para-lib';` : '/* dynamic 为动态代码注释,请勿删除 */';
-            break;
-        case 'Debugger':
-            code = env === 'dev' ? `await Debugger.init(debug);` : '/* dynamic 为动态代码注释,请勿删除 */';
-            break;
-        case 'init':
-            code = env === 'dev' ? `init(${JSON.stringify(dev.debug || [])})` : 'init();';
-            break;
-        case 'version':
-            code = `window.ParaWeb = {version: '${version}', env: '${env || ''}', buildTime: '${new Date().toLocaleString()}'}`;
-            break;
-    }
-    return `${s1}${code}${s4}`;
+  let code = '';
+  switch (s2) {
+    case 'debug':
+      code = env === 'dev' ? `import {Debugger, Context} from 'para-lib';` : `import {Context} from 'para-lib';`;
+      break;
+    case 'Debugger':
+      code = env === 'dev' ? `await Debugger.init(debug);` : 'await Context.load();';
+      break;
+    case 'init':
+      code = env === 'dev' ? `init(${JSON.stringify(dev.debug || [])})` : 'init();';
+      break;
+    case 'version':
+      code = `window.ParaWeb = {version: '${version}', env: '${env || ''}', buildTime: '${new Date().toLocaleString()}'}`;
+      break;
+  }
+  return `${s1}${code}${s4}`;
 });
 try {
-    fs.writeFileSync(indexPath, index);
+  fs.writeFileSync(indexPath, index);
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
 console.log('正在启动 react-cli ...');
